a4578afd9c924283189a5e85ab5125504ce02243
amir.liu <amir.liu@chixi88.com>
2024-03-25 09:48:19 +0800

    feat: 使用sass管理模块化样式

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 import 'styles/global.css'
+import 'styles/custom.scss'
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
@@ -34,7 +35,7 @@ export default function App({ Component, pageProps }: AppProps) {
     function onRouteChangeComplete() {
       if (fathomId) {
         Fathom.trackPageview()
-      } 
+      }
 
       if (posthogId) {
         posthog.capture('$pageview')
