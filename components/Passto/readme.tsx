10d0bf614f968853ae27e46f39f28b9dca5e0e75
luffy.luo <luffy.luo@chixi88.com>
2024-04-07 20:08:10 +0800

    fix: 更新域名

--- components/Passto/readme.tsx
+++ components/Passto/readme.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 
+import { domain } from '@/lib/config'
+
 export function PasstoReadme(props) {
   return (
     <iframe
-      src={`https://passto2pay.zapto.org${props.meta.url}`}
+      src={`https:/${domain}${props.meta.url}`}
       width='100%'
       height='1000'
       style={{ marginTop: '-64px', overflow: 'hidden' }}
