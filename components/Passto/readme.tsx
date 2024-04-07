619926a8b4904e7491b03e43169db3266ef7a41f
Luffy.Luo <luffy.luo@chixi88.com>
2024-04-07 22:32:56 +0800

    fix: 更新readme

--- components/Passto/readme.tsx
+++ components/Passto/readme.tsx
@@ -5,7 +5,7 @@ import { domain } from '@/lib/config'
 export function PasstoReadme(props) {
   return (
     <iframe
-      src={`https:/${domain}${props.meta.url}`}
+      src={`https://${domain}${props.meta.url}`}
       width='100%'
       height='1000'
       style={{ marginTop: '-64px', overflow: 'hidden' }}
