b394a63a1ce15a924dc0222f8dcaf0a0c2b89993
amir.liu <amir.liu@chixi88.com>
2024-07-25 13:07:24 +0800

    feat: 添加hotjar统计代码

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -79,6 +79,12 @@ export default function App({ Component, pageProps }: AppProps) {
         }
         `}
       </Script>
+
+      <Script>
+        {`
+          (function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:5070229,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </Script>
     </SnackbarProvider>
   )
 }
